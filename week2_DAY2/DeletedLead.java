a509a733afdd15a3678211c5d0399eabb95fae2f
santhosh-pachaiappan <117085987+santhosh-pachaiappan@users.noreply.github.com>
2022-11-11 13:32:43 +0530

    Add files via upload

--- week2_DAY2/DeletedLead.java
+++ week2_DAY2/DeletedLead.java
@@ -1,9 +1,42 @@
 package week2_DAY2;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DeletedLead {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			WebDriverManager.chromedriver().setup();
+			ChromeDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("http://leaftaps.com/opentaps/");
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+			driver.findElement(By.id("password")).sendKeys("crmsfa");
+			driver.findElement(By.className("decorativeSubmit")).click();
+			driver.findElement(By.linkText("CRM/SFA")).click();
+			driver.findElement(By.linkText("Leads")).click();
+			System.out.println(driver.getTitle());
+			driver.findElement(By.linkText("Find Leads")).click();
+			System.out.println(driver.getTitle());
+			driver.findElement(By.xpath("//span[text()='Phone']")).click();
+			driver.findElement(By.name("phoneNumber")).sendKeys("0123444544");
+			driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+			driver.findElement(By.linkText("Santosh")).click();
+			String ssew = driver.findElement(By.cssSelector("span#viewLead_companyName_sp")).getText();
+			System.out.println(ssew);
+			driver.findElement(By.linkText("Delete")).click();
+			driver.findElement(By.linkText("Find Leads")).click();
+			driver.findElement(By.name("id")).sendKeys("12944");
+			driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+			System.out.println(driver.findElement(By.id("ext-gen437")).getText());
+			
+		
 
 	}
 
--- week2_DAY2/Facebook.java
+++ week2_DAY2/Facebook.java
@@ -1,9 +1,39 @@
 package week2_DAY2;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Facebook {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://en-gb.facebook.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.linkText("Create New Account")).click();
+		driver.findElement(By.name("firstname")).sendKeys("santhosh");
+		driver.findElement(By.name("lastname")).sendKeys("sv");
+		driver.findElement(By.name("reg_email__")).sendKeys("sdfdfd");
+		driver.findElement(By.name("reg_passwd__")).sendKeys("Santhosh123");
+		WebElement birthdayday = driver.findElement(By.name("birthday_day")); 
+		Select day = new Select(birthdayday); 
+		day.selectByValue("5");
+		WebElement birthdaymonth = driver.findElement(By.name("birthday_month")); 
+		Select month = new Select(birthdaymonth);
+		month.selectByVisibleText("dec"); 
+		WebElement birthdayyear = driver.findElement(By.name("birthday_year")); 
+		Select year = new Select(birthdayyear);
+		year.selectByValue("1996"); 
+		driver.findElement(By.xpath("(//input[@name='sex'])[2]")).click();
+		driver.findElement(By.name("websubmit")).click();
 
 	}
 
